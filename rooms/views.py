495ca48d04f8b10ed9518f64b7c12347f8f44196
avierkin03 <avierkin03@gmail.com>
2026-01-10 15:33:44 +0200

    10.01.2026

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Room, Booking
 
-# Create your views here.
+
+# Функція представлення списку всіх кімнат
+def rooms_list(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms  
+    }
+    return render(request=request, template_name="rooms/room_list.html", context=context)
