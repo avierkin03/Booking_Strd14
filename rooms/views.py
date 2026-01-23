ea7806f51412e5a60a586c06cfd54b02e9292631
avierkin03 <avierkin03@gmail.com>
2026-01-23 14:16:28 +0200

    17/01/2025

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from .models import Room, Booking
 
-
 # Функція представлення списку всіх кімнат
 def rooms_list(request):
     rooms = Room.objects.all()
@@ -9,3 +8,19 @@ def rooms_list(request):
         "rooms": rooms  
     }
     return render(request=request, template_name="rooms/room_list.html", context=context)
+
+
+# Функція представлення головної сторінки та пошуку кімнат
+def main_page(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms,
+        "start_date": "",
+        "end_date": "",
+        "min_price": "",
+        "max_price": "",
+        "capacity": "",
+    }
+
+    return render(request=request, template_name="rooms/main_page.html", context=context)
+
