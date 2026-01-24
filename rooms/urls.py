4a93cd6c3bfdd687f959a6ae8a6e00e59c039a0b
avierkin03 <avierkin03@gmail.com>
2026-01-24 15:34:04 +0200

    24/01/2026

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.main_page, name="main_page"),
     path("rooms/", views.rooms_list, name="rooms"),
+    path("booking/", views.book_room, name="booking"),
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -22,5 +22,54 @@ def main_page(request):
         "capacity": "",
     }
 
+    if request.method == "POST":
+        # отримуємо дані з POST-запиту
+        start_date = request.POST.get("start_date")
+        end_date = request.POST.get("end_date")
+        capacity = request.POST.get("capacity")
+        min_price = request.POST.get("min_price")
+        max_price = request.POST.get("max_price")
+
+        if start_date and end_date:
+            # залишаємо вільні номери
+            rooms = rooms.exclude(bookings__start_time__lt = end_date, bookings__end_time__gt = start_date)
+            # враховуэмо додаткові фільтри, якщо вони вказані
+            if capacity:
+                rooms = rooms.filter(capacity__gte=capacity)
+            if min_price:
+                rooms = rooms.filter(price__gte=min_price)
+            if max_price:
+                rooms = rooms.filter(price__lte=max_price)
+        
+        context = {
+            "rooms": rooms,
+            "start_date": start_date,
+            "end_date": end_date,
+            "min_price": min_price,
+            "max_price": max_price,
+            "capacity": capacity,
+        }
+
     return render(request=request, template_name="rooms/main_page.html", context=context)
+
+
+# Функція представлення бронювання кімнати
+def book_room(request):
+    if request.method == "GET":
+        room_id = request.GET.get("room_id")
+        start_date = request.GET.get("start_date")
+        end_date = request.GET.get("end_date")
+
+        room = Room.objects.get(id = room_id)
+        context = {
+            "room": room,
+            "start_date": start_date,
+            "end_date": end_date
+        }
+
+        return render(request=request, template_name="rooms/booking.html", context=context)
+    
+    elif request.method == "POST":
+        pass
+
 
