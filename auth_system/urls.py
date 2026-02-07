cc99f32d0937a2b370e65c4c81c8bc139d549c54
avierkin03 <avierkin03@gmail.com>
2026-02-07 14:45:52 +0200

    user login

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("logout/", views.user_logout, name="logout"),
+    path("login/", views.user_login, name="login"),
 ]
--- auth_system/views.py
+++ auth_system/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 # Логаут користувача
 def user_logout(request):
     logout(request)
     return redirect("main_page")
+
+
+# Логін користувача
+def user_login(request):
+    if request.method == "GET":
+        form = AuthenticationForm()
+
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username") 
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("main_page")
+
+    return render(request, template_name="auth_system/login.html", context={"form": form})
+
